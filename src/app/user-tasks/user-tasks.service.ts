820525a119b242dc07559b6bcd14b02d8636eebb
Marco Colia <79002016+justoverclockl@users.noreply.github.com>
2025-02-21 18:03:01 +0100

    add localstorage to persist

--- src/app/user-tasks/user-tasks.service.ts
+++ src/app/user-tasks/user-tasks.service.ts
@@ -29,6 +29,14 @@ export class UserTasksService {
     },
   ]
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks')
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks)
+    }
+  }
+
   getUserTasks(userId:string) {
     return this.tasks.filter(task => task.userId === userId)
   }
@@ -41,9 +49,15 @@ export class UserTasksService {
       dueDate: taskData.dueDate,
       userId: userId
     })
+    this.saveTasks()
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id)
+    this.saveTasks()
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
   }
 }
